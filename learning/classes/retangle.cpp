2e7ed5acbb3d5111bda07303531029b0aa6cb214
shelsonx <shelsonx@gmail.com>
2023-05-15 23:40:10 -0400

    add constructors

--- learning/classes/retangle.cpp
+++ learning/classes/retangle.cpp
@@ -3,9 +3,16 @@
 class Rectangle {
     int width, height;
     public:
+        Rectangle();
         Rectangle(int, int);
         double area(void);
 };
+
+Rectangle::Rectangle()
+{
+    width = 5;
+    height = 5;
+}
 
 Rectangle::Rectangle (int x, int y)
 {
@@ -20,13 +27,8 @@ double Rectangle::area()
 
 int main (void)
 {
-    int x, y;
-
-    std::cout << "Enter x: ";
-    std::cin >> x;
-
-    std::cout << "Enter y: ";
-    std::cin >> y;
-    Rectangle rect(x, y);
-    std::cout << rect.area() << std::endl;
+    Rectangle rect_a(10, 15);
+    Rectangle rect_b;
+    std::cout << "Area rect a: " << rect_a.area() << std::endl;
+    std::cout << "Area rect b: " << rect_b.area() << std::endl;
 }
