63cb9f7417f71f79a796f89b51d4235b3f104d5f
shelsonx <shelsonx@gmail.com>
2023-06-02 11:42:46 -0400

    remove getIndex function

--- MODULE_01/ex05/Harl.cpp
+++ MODULE_01/ex05/Harl.cpp
@@ -2,54 +2,49 @@
 
 Harl::Harl(){}
 
-static int getLevelIndex(const std::string level)
-{
-    std::string harlsNames[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    for (int i=0; i < 4; i++)
-    {
-        if (level.compare(harlsNames[i]) == 0)
-            return i;
-    }
-    return -1;
-}
-
 void Harl::complain(std::string level)
 {
+    int index = -1;
+    std::string harlsNames[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
     void (Harl::*harls[4])(void) = {
         &Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
     
-    int index = getLevelIndex(level);
+    for (int i=0; i < 4; i++)
+    {
+        if (level.compare(harlsNames[i]) == 0)
+            index = i;
+    }
     if (index != -1)
         (this->*harls[index])();
 }
 
 void Harl::debug(void)
 {
-    std::cout << "[DEBUG]: " << std::endl <<
+    std::cout << "[ DEBUG ]: " << std::endl <<
         "I love having extra bacon for my "
         "7XL-double-cheese-triple-pickle-specialketchup burger. "
-        "I really do!" << std::endl;
+        "I really do!" << std::endl << std::endl;
 }
 
 void Harl::info(void)
 {
-    std::cout << "[INFO]: " << std::endl <<
+    std::cout << "[ INFO ]: " << std::endl <<
         "I cannot believe adding extra bacon costs more money. "
         "You didn’t put enough bacon in my burger! If you did, "
-        "I wouldn’t be asking for more!" << std::endl;
+        "I wouldn’t be asking for more!" << std::endl << std::endl;
 }
 
 void Harl::warning(void)
 {
-    std::cout << "[WARNING]: " << std::endl <<
+    std::cout << "[ WARNING ]: " << std::endl <<
         "I think I deserve to have some extra bacon for free." 
         "I’ve been coming for years whereas you started "
-        "working here since last month." << std::endl;
+        "working here since last month." << std::endl << std::endl;
 }
 
 void Harl::error(void)
 {
-    std::cout << "[ERROR]: " << std::endl <<
+    std::cout << "[ ERROR ]: " << std::endl <<
         "This is unacceptable! I want to speak to the manager now."
-        << std::endl;
+        << std::endl << std::endl;
 }
