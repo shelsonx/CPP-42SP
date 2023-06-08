4791c7d2208172b05252dfa40bccacd6b1de8816
shelsonx <shelsonx@gmail.com>
2023-06-08 15:16:03 -0400

    completed tests

--- MODULE_02/ex02/tests.cpp
+++ MODULE_02/ex02/tests.cpp
@@ -12,20 +12,57 @@ struct MyFixture {
     }
 };
 
-TEST_CASE_FIXTURE(MyFixture, "sum") {
+TEST_CASE_FIXTURE(MyFixture, "arithmetic operators") {
   
-    Fixed result = a + b;
-    CHECK(result == 4);
+    Fixed sum = a + b;
+    Fixed sub = a - b;
+    Fixed mul = a * b;
+    Fixed div = a / b;
+
+    CHECK(sum == 4);
+    CHECK(sub == 0);
+    CHECK(mul == 4);
+    CHECK(div == 1);
 }
 
-TEST_CASE_FIXTURE(MyFixture, "sub") {
-  
-    Fixed result = a - b;
-    CHECK(result == 0);
+TEST_CASE_FIXTURE(MyFixture, "increments and decrements") {
+    a = 3;
+    b = 3;
+    CHECK(++a == ++b);
+    a++;
+    b++;
+    CHECK(a == b);
+    CHECK(--a == --b);
+    a--;
+    b--;
+    CHECK(a == b);
 }
 
-TEST_CASE_FIXTURE(MyFixture, "mul") {
-  
-    Fixed result = a * b;
-    CHECK(result == 4);
+TEST_CASE_FIXTURE(MyFixture, "comparison operators") {
+    a = 2;
+    b = 1;
+    std::string result1 = a > b ? "true" : "false";
+    std::string result2 = b < a ? "true" : "false";
+    b = 2;
+    std::string result3 = b >= a ? "true" : "false";
+    std::string result4 = b <= a ? "true" : "false";
+    std::string result5 = b == a ? "true" : "false";
+    std::string result6 = b != a ? "true" : "false";
+    CHECK(result1 == "true");
+    CHECK(result2 == "true");
+    CHECK(result3 == "true");
+    CHECK(result4 == "true");
+    CHECK(result5 == "true");
+    CHECK(result6 == "false");
+}
+
+TEST_CASE_FIXTURE(MyFixture, "min and max") {
+    Fixed const c = 2;
+    Fixed const d = 3;
+
+    Fixed const min = Fixed::min( c, d );
+    CHECK_EQ(min.toFloat(), 2.0);
+
+    Fixed const max = Fixed::max( c, d );
+    CHECK_EQ(max.toFloat(), 3.0);
 }
