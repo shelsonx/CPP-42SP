74a56b5c9a7839f9794c70952ae93823e92b69c6
shelsonx <shelsonx@gmail.com>
2023-06-20 23:02:31 -0400

    create Wrong classes

--- MODULE_04/ex00/main.cpp
+++ MODULE_04/ex00/main.cpp
@@ -1,20 +1,38 @@
+#include <iomanip>
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
 
-int main (void) {
-    const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << meta->getType() << " " << std::endl;
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound(); //will output the cat sound!
-    j->makeSound();
-    meta->makeSound();
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
 
-    delete meta;
-    delete i;
-    delete j;
+int main (void) {
+    {
+        const Animal* meta = new Animal();
+        const Animal* j = new Dog();
+        const Animal* i = new Cat();
+        std::cout << meta->getType() << " " << std::endl;
+        std::cout << j->getType() << " " << std::endl;
+        std::cout << i->getType() << " " << std::endl;
+        i->makeSound(); //will output the cat sound!
+        j->makeSound();
+        meta->makeSound();
+
+        delete meta;
+        delete i;
+        delete j;
+    }
+    std::cout << std::setw(40) << std::setfill('*') << "" << std::endl;
+    {
+        const WrongAnimal *wanimal = new WrongAnimal();
+        const WrongAnimal *wcat = new WrongCat();
+        std::cout << wcat->getType() << " " << std::endl;
+        std::cout << wanimal->getType() << " " << std::endl;
+        wcat->makeSound();
+        wanimal->makeSound();
+
+        delete wcat;
+        delete wanimal;
+    }
     return 0;
 }
