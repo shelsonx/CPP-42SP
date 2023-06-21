16d662a99a528df9ef4fe517a86ab28af4a90705
shelsonx <shelsonx@gmail.com>
2023-06-20 20:37:40 -0400

    create class Cat

---
+++ MODULE_04/ex00/Cat.cpp
@@ -0,0 +1,26 @@
+#include "Cat.hpp"
+
+Cat::Cat() {
+    std::cout << "Cat: Default constructor called!" << std::endl;
+    _type = "Cat";
+}
+
+Cat::Cat(const Cat& other) : Animal() {
+    std::cout << "Cat: Copy constructor called!" << std::endl;
+    _type = other._type;
+}
+
+Cat& Cat::operator=(const Cat& other) {
+    std::cout << "Cat: Copy assignment operator called!" << std::endl;
+    if (this != &other)
+        _type = other._type;
+    return *this;
+}
+
+Cat::~Cat() {
+    std::cout << "Cat: Default destructor called!" << std::endl;
+}
+
+void Cat::makeSound() {
+    std::cout << "Meow, Meow..." << std::endl;
+}
--- MODULE_04/ex00/main.cpp
+++ MODULE_04/ex00/main.cpp
@@ -1,8 +1,13 @@
 #include "Animal.hpp"
+#include "Cat.hpp"
 
 int main (void) {
     Animal animal;
+    Cat cat;
 
     std::cout << animal.getType() << std::endl;
     animal.makeSound();
+
+    std::cout << cat.getType() << std::endl;
+    cat.makeSound();
 }
