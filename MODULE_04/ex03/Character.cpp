551dba106d39bad2198bc24e52252ebd068ae8c0
shelsonx <shelsonx@gmail.com>
2023-06-23 23:41:16 -0400

    create and implemented Character class

---
+++ MODULE_04/ex03/Character.cpp
@@ -0,0 +1,91 @@
+#include "Character.hpp"
+
+Character::Character(const std::string& name) {
+    std::cout << "Character: type with parameter constructor called!" << std::endl;
+    this->_name = name;
+    this->initInventory();
+}
+
+Character::Character() {
+    std::cout << "Character: Default constructor called!" << std::endl;
+    this->_name = "Default";
+    this->initInventory();
+}
+
+Character::Character(const Character& other) {
+    std::cout << "Character: Copy constructor called!" << std::endl;
+    *this = other;
+}
+
+Character::~Character() {
+    std::cout << "Character: Default destructor called!" << std::endl;
+    this->emptyInventory();
+}
+
+Character& Character::operator=(const Character& other) {
+    std::cout << "Character: Copy assignment operator called!" << std::endl;
+    if (this != &other) {
+        this->_name = other.getName();
+        this->emptyInventory();
+        for (int i = 0; i < MAX_SLOTS; i++) {
+            this->_inventory[i] = other._inventory[i]->clone();
+        }
+    }
+    return *this;
+}
+
+void Character::equip(AMateria* m) {
+    if (!m) {
+        std::cout << m << " Not valid!" << std::endl;
+        return ;
+    }
+    for (int i = 0; i < MAX_SLOTS; i ++) {
+        if (this->_inventory[i] != NULL) {
+            this->_inventory[i] = m;
+            std::cout << "successfully equipped!" << std::endl;
+            return ;
+        }
+    }
+    std::cout << "Full inventory!" << std::endl;
+}
+
+void Character::unequip(int idx) {
+    if (this->_inventory[idx] == NULL) {
+        std::cout << idx << " Not found!" << std::endl;
+        return ;
+    }
+    if (idx < 0 || idx >= MAX_SLOTS) {
+        std::cout << idx << " out of range!" << std::endl;
+        return ;
+    }
+    this->_inventory[idx] = NULL;
+    std::cout << idx << "Successfully unequipped!" << std::endl;
+}
+
+void Character::use(int idx, ICharacter& target) {
+    if (this->_inventory[idx] == NULL) {
+        std::cout << idx << " Not found!" << std::endl;
+        return ;
+    }
+    if (idx < 0 || idx >= MAX_SLOTS) {
+        std::cout << idx << " out of range!" << std::endl;
+        return ;
+    }
+    this->_inventory[idx]->use(target);
+}
+
+std::string const & Character::getName() const {
+    return this->_name;
+}
+
+void Character::initInventory() {
+    for (int i = 0; i < MAX_SLOTS; i++)
+        this->_inventory[i] = NULL;
+}
+
+void Character::emptyInventory() {
+    for (int i = 0; i < MAX_SLOTS; i++) {
+        delete this->_inventory[i];
+        this->_inventory[i] = NULL;
+    }
+}
--- MODULE_04/ex03/Character.hpp
+++ MODULE_04/ex03/Character.hpp
@@ -6,20 +6,24 @@
 
 class Character : public ICharacter {
     public:
+        Character(const std::string& name);
         Character();
         Character(const Character& other);
         Character& operator=(const Character& other);
         ~Character();
 
-        std::string const & getName() const = 0;
-        void equip(AMateria* m) = 0;
-        void unequip(int idx) = 0;
-        void use(int idx, ICharacter& target) = 0;
+        std::string const & getName() const;
+        void equip(AMateria* m);
+        void unequip(int idx);
+        void use(int idx, ICharacter& target);
     
     private:
-        static const int SLOTS = 4;
-        AMateria* slots[SLOTS];
+        static const int MAX_SLOTS = 4;
+        AMateria* _inventory[MAX_SLOTS];
         std::string _name;
+
+        void initInventory();
+        void emptyInventory();
 };
 
 #endif
